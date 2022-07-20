18deff06d09816466a5ac0f019ad49ef2a0acc31
Zen Nimorakiotakis <nimos.zen@gmail.com>
2022-07-20 16:59:30 +1000

    login validation

--- login.php
+++ login.php
@@ -14,30 +14,27 @@
 
         $error = null;
 
-        if (isset($_POST['username']) && isset($_POST['password']))  {
+        if (empty($_POST['username']) || empty($_POST['password']))  { // existance check
+            $error = 'please enter account details';
+        } else {
             $username = $_POST['username'];
             $pw = $_POST['password'];
-        } else {
-            $error = 'please enter account details';
-            exit;
-        }
 
-        // check if username existsTODO put in if statement so this doesnt run if username and password dont exist
-        $file = new SplFileObject("data\\users.csv");
-        $file -> setFlags(SplFileObject::READ_CSV|SplFileObject::SKIP_EMPTY);
+            $file = new SplFileObject("data\\users.csv");
+            $file -> setFlags(SplFileObject::READ_CSV|SplFileObject::SKIP_EMPTY);
 
-        $error = "Username or password not correct.";
-        foreach ($file as $user) {
-            if (!empty($user)) {
-                if (strcasecmp($user[0], $username) == 0 && password_verify($pw, $user[1])) {
-                    $error = null;
-                    break;
+            $error = "Username or password not correct.";
+            foreach ($file as $user) {
+                if (!empty($user)) {
+                    if (strcasecmp($user[0], $username) == 0 && password_verify($pw, $user[1])) {
+                        $error = null;
+                        break;
+                    }
                 }
             }
+
+            $file = null;
         }
-
-        $file = null;
-
 
         if (empty($error)) {
             //log em in, initiate session variables
@@ -58,9 +55,9 @@
     <form method="post">
         <h3>Login</h3>
         <label for="username">Username:</label>
-        <input type="text" name="username" id="username" placeholder="username" >
+        <input type="text" name="username" id="username" placeholder="username" required>
         <label for="password">Password:</label>
-        <input type="password" name="password" id="password" placeholder="password" >
+        <input type="password" name="password" id="password" placeholder="password" required>
         <button type="submit" name="login">Login</button>
         <br><br>
         <p>Don't have an account? <a href="register.php">Register</a> now!</p>
